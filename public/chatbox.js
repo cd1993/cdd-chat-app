2e164cae373b0ac520166c54a425bd51273fb7ef
Constantine Doukas <cdoukas2017@gmail.com>
2019-01-31 01:45:05 -0500

    Editing styles, adding event listener for the send button, editing frontend JS to handle the event - 1-31-19

--- public/chatbox.js
+++ public/chatbox.js
@@ -1,2 +1,16 @@
 // Make connection on the frontend
-var socket = io.connect('http://localhost:182');
+let socket = io.connect('http://localhost:182');
+
+// Query DOM for inputs
+let message = document.getElementById('chat-message'),
+    username = document.getElementById('chat-username'),
+    sendButton = document.getElementById('chat-send'),
+    chatOutput = document.getElementById('chat-output');
+
+// Emit an event when user clicks send
+sendButton.addEventListener('click', (event) => {
+  socket.emit('chatMessage', { // Emit the message to the backend
+    message: message.nodeValue,
+    username: username.value
+  }); 
+});
