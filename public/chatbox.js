e1da1e3ce346947342f24a401843d5adb44344ab
Constantine Doukas <cdoukas2017@gmail.com>
2019-01-31 01:57:11 -0500

    Changing some JS and html. Can't figure out why the form is still refreshing the page on submit. some form event listener problem - 1-31-19

--- public/chatbox.js
+++ public/chatbox.js
@@ -4,13 +4,17 @@ let socket = io.connect('http://localhost:182');
 // Query DOM for inputs
 let message = document.getElementById('chat-message'),
     username = document.getElementById('chat-username'),
-    sendButton = document.getElementById('chat-send'),
-    chatOutput = document.getElementById('chat-output');
+    chatOutput = document.getElementById('chat-output'),
+    chatForm = document.getElementById('chat-form');
 
 // Emit an event when user clicks send
-sendButton.addEventListener('click', (event) => {
+chatForm.submit ( (event) => {
+  event.preventDefault();
+
   socket.emit('chatMessage', { // Emit the message to the backend
     message: message.nodeValue,
     username: username.value
   }); 
+
+  chatForm.reset();
 });
