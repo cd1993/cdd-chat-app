17db1a279a18823b282a346a657a14832d7afecd
Constantine Doukas <cdoukas2017@gmail.com>
2019-02-06 17:03:01 -0500

    Adding readme, catching up with project, now about to continue emitting mesages feature - 2-6-19

---
+++ README.md
@@ -0,0 +1,9 @@
+## Web Sockets using Node and Socket.io
+
+### About the app:
+a) uses Node and Socket.io
+b) Currently the frontend is built simply without libraries or frameworks
+
+### Starting the app:
+1) run nodemon (currently the port is 182, so the app is running at localhost:182)
+
--- public/chatbox.js
+++ public/chatbox.js
@@ -1,34 +1,28 @@
 
 
-
-document.addEventListener('DOMContentLoaded', function () {
-
+function handleChat() {
   // Make connection on the frontend
-var socket = io.connect('http://localhost:182');
+  var socket = io.connect('http://localhost:182');
 
-// Query DOM for inputs
-var message = document.getElementById('chat-message'),
-    username = document.getElementById('chat-username'),
-    chatOutput = document.getElementById('chat-output'),
-    chatForm = document.getElementById('chat-form'),
-    sendButton = document.getElementById('chat-send');
-
-
-// Emit an event when user clicks send
-sendButton.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  socket.emit('chatMessage', { // Emit the message to the backend. Now we need to handle the data on the backend
-    message: message.value,
-    username: username.value
-  }); 
-
-
-});
-
-
-});
-
+  // Query DOM for inputs
+  var message = document.getElementById('chat-message'),
+      username = document.getElementById('chat-username'),
+      chatOutput = document.getElementById('chat-output'),
+      chatForm = document.getElementById('chat-form'),
+      sendButton = document.getElementById('chat-send');
+  
+  // Emit an event when user clicks send
+  sendButton.addEventListener('click', function(event) {
+    event.preventDefault();
+  
+    socket.emit('chatMessage', { // Emit the message to the backend. Now we need to handle the data on the backend
+      message: message.value,
+      username: username.value
+    }); 
+  });  
+}
+/* Run handleChat function on DOMContentLoaded */
+document.addEventListener('DOMContentLoaded', handleChat());
 
 
 
